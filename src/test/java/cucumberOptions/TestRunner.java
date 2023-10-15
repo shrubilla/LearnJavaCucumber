5224aa64d7c5663123b4c3f24fac2a5cd502e7df
shruthi <shruthi.bk99@gmail.com>
2023-10-15 09:11:28 +0530

    update notation

--- src/test/java/cucumberOptions/TestRunner.java
+++ src/test/java/cucumberOptions/TestRunner.java
@@ -3,8 +3,8 @@ package cucumberOptions;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
-@CucumberOptions(features="src/test/java/features",tags="@Login",
-                  glue="stepDefinitions",
+@CucumberOptions(features="src/test/java/features",tags="@Search or @Filter",
+                  glue="stepDefinitions",plugin = { "pretty","html:target/cucumber-reports" },
                   monochrome=true)
 public class TestRunner extends AbstractTestNGCucumberTests {
 
